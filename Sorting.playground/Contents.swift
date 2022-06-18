7c2f9da8502d4a046ef4d9aa79de95d408303486
christo <christoslimit@gmail.com>
2022-06-18 22:07:02 +0530

    Sorting implemented

--- Sorting.playground/Contents.swift
+++ Sorting.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Bubble Sort
+func bubbleSort(_ array: inout [Int]) {
+    for p1 in 0..<array.count {
+        for p2 in p1..<array.count {
+            if(array[p1] > array[p2]) {
+                let temp = array[p1]
+                array[p1] = array[p2]
+                array[p2] = temp
+            }
+        }
+    }
+    
+}
+var array = [1,5,3,10,4,9]
+bubbleSort(&array)
+print(array)
+
