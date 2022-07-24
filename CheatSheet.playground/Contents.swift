5a91a04bf1afa95c38a4173a83b73021bb2cf90a
christo <christoslimit@gmail.com>
2022-07-24 15:18:57 +0530

    Optimised the cheatsheet.

--- CheatSheet.playground/Contents.swift
+++ CheatSheet.playground/Contents.swift
@@ -1,59 +1,85 @@
 import UIKit
 import Darwin
 
-/// Array ///
-var array = [1,3,5,7,9]
-var propArray: Array<Int> = Array<Int>()
+///Array
+var fruits = ["Apple", "Banana", "Cherry"]
+var fruits1: Array<String> = Array<String>()
 
-//Operation
-array.append(1)
-array.append(contentsOf: [9,10])
-array.insert(100, at: 0)
-array[0] = 200
-array.remove(at: 0)
+//Operations
+//Appending
+fruits1.append("Guava")
+fruits1.append("Mango")
+fruits.append(contentsOf: fruits1)
 
-//Iteration Workflow
-for i in array { print(i) }
-for i in 0..<array.count {print(i) }
-for (i, v) in array.enumerated() { print("\(i) \(v)")}
-for i in stride(from: 0, to: array.count , by:1) { print(i) }
-var itr = array.makeIterator()
-while let i = itr.next() { print(i)}
+//Insert
+fruits.insert("Guava", at: 0)
 
-//Hoc
-array.forEach { i in print(i) }
-let mappedArray = array.map { num in
-    return num * num
+//Delete
+fruits.remove(at: 0)
+
+//find ~ firstIndex, first
+if let index = fruits.firstIndex(where: {$0 == "Apple"}) {
+   print(index)
 }
-print(mappedArray)
-let filtered = array.filter { num in
-    return num >= 3
+if (fruits.contains("Apple")) {
+    print("Element exsists")
 }
-print(filtered)
-let reduced = array.reduce(into: 0) { s, v in
-    return s = s + v
+
+//Slicing
+let fruits2 = fruits.prefix(2)
+print(fruits2)
+let fruits3 = fruits.suffix(2)
+print(fruits3)
+
+let start = fruits.index(fruits.startIndex, offsetBy: 1)
+let end = fruits.index(fruits.endIndex, offsetBy: -1)
+let range = start...end
+let fruits4 = fruits[range]
+print(fruits4)
+print(fruits)
+
+//Iteration
+//for in value
+for fruit in fruits { print(fruit) }
+//for in index
+for index in 0..<fruits.count { print(index) }
+//for in i,v
+for (i,v) in fruits.enumerated() {print("\(i) - \(v)")}
+//for stride through
+for i in stride(from: 0, through: fruits.count, by: 1){print(i)}
+//iterator
+var itr = fruits.makeIterator()
+while let v = itr.next() { print(v) }
+
+//HOC
+fruits.forEach { fruit in print(fruit) }
+let mappedFruits = fruits.map { fruit in
+    return  fruit.appending("s")
 }
-print(reduced)
-let sorted = array.sorted { a, b in
+print(mappedFruits)
+let filteredArray = fruits.filter { fruit in
+    return fruit != "Apple"
+}
+print(filteredArray)
+
+let sorted = fruits.sorted { a, b in
     return a > b
 }
+print("Sorting")
 print(sorted)
 
+let reduced = fruits.reduce(into: "") { s, v in
+    return s.append(v)
+}
+print(reduced)
+
+/*
 ////////////// String /////////
 var greeting = "Hello, playground"
-print(greeting.prefix(5))
-print(greeting.suffix(5))
-let startIndex = greeting.index(greeting.startIndex, offsetBy: 5)
-let endIndex = greeting.index(greeting.endIndex, offsetBy: -2)
-let range = startIndex...endIndex
-let subgreeting = greeting[range]
-print(subgreeting)
-greeting.isEmpty
-greeting.count
 let charArray = Array(greeting)
 print(charArray)
 let str2 = String(charArray)
-
+*/
 ////////// Map ///////
 var movieMap = ["DCH":  1]
 var someMap: Dictionary<String, Int> = Dictionary<String, Int> ()
