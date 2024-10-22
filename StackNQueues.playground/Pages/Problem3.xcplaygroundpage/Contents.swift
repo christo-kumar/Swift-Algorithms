c102fc83e6e6c716bfc9b5d1f28caff3148b3bb3
Christo Kumar <christoslimit@gmail.com>
2024-10-22 20:09:28 +0530

    tallest build solved

--- StackNQueues.playground/Pages/Problem3.xcplaygroundpage/Contents.swift
+++ StackNQueues.playground/Pages/Problem3.xcplaygroundpage/Contents.swift
@@ -4,4 +4,18 @@ import Foundation
 
 var greeting = "Hello, playground"
 
+class Program {
+    func sunsetViews(_ buildings: [Int], _ direction: String) -> [Int] {
+        // Write your code here.
+        var answer = Array<Int>()
+        var tallest = 0
+        let iterator = direction == "WEST" ? stride(from: 0, to: buildings.count, by:1) : stride(from: buildings.count - 1, to: -1, by: -1)
+        for i in iterator where buildings[i] > tallest {
+            answer.append(i)
+            tallest = buildings[i]
+        }
+        return direction == "WEST" ? answer : answer.reversed()
+    }
+}
+
 //: [Next](@next)
