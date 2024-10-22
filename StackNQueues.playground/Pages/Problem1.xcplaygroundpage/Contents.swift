cf18d7aec74d599c22ddb643d92b32d2181f8186
Christo Kumar <christoslimit@gmail.com>
2024-10-22 17:27:17 +0530

    problems for stack and queues

--- StackNQueues.playground/Pages/Problem1.xcplaygroundpage/Contents.swift
+++ StackNQueues.playground/Pages/Problem1.xcplaygroundpage/Contents.swift
@@ -1,3 +1,42 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "min max stack"
+
+class Program {
+  // Feel free to add new properties and methods to the class.
+  class MinMaxStack {
+    private var elements: [Int] = []
+    private var maximum: [Int] = []
+    private var minimum: [Int] = []
+    
+    func peek() -> Int? {
+      // Write your code here.
+      return elements.last
+    }
+
+    func pop() -> Int? {
+      // Write your code here.
+      minimum.popLast()
+      maximum.popLast()
+      return elements.popLast()
+    }
+
+    func push(number: Int) {
+      // Write your code here.
+      elements.append(number)
+      minimum.append(min(minimum.last ?? number, number))
+      maximum.append(max(maximum.last ?? number, number))
+    }
+
+    func getMin() -> Int? {
+      // Write your code here.
+      return minimum.last
+    }
+
+    func getMax() -> Int? {
+      // Write your code here.
+      return maximum.last
+    }
+  }
+}
+
--- StackNQueues.playground/Pages/Problem2.xcplaygroundpage/Contents.swift
+++ StackNQueues.playground/Pages/Problem2.xcplaygroundpage/Contents.swift
@@ -4,4 +4,23 @@ import Foundation
 
 var greeting = "Hello, playground"
 
+class Program {
+    func balancedBrackets(string: String) -> Bool {
+        // Write your code here.
+        //We will push only the opening brackets to the stack
+        //and only validate the sequence of closing brackets
+        let openingBrackets: Set<Character> = ["(","[","{"]
+        let matchingBrackets: [Character: Character] = [")":"(","]":"[","}":"{"]
+        var stack = [Character]()
+        for c in string {
+            if openingBrackets.contains(c) {
+                stack.append(c)
+            } else if let openingBracket = matchingBrackets[c] {
+                guard stack.popLast() == openingBracket else { return false }
+            }
+        }
+        return stack.isEmpty
+    }
+}
+
 //: [Next](@next)
